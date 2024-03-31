ac5d0daa9acc6ba43f754bbd89748801c3f37635
ksdev <ks.cloudbox@gmail.com>
2024-03-31 20:42:21 +0200

    Initial layout

--- src/layout/LayoutContents.vue
+++ src/layout/LayoutContents.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
+import {ref} from 'vue';
 
+const input = ref('');
+
+function onClick() {
+  alert(input.value);
+}
 </script>
 
 <template>
   <div class="flex flex-1 flex-col overflow-auto">
     <main class="flex-1 p-4 overflow-auto">
-      Chat
+      Input: {{ input }}
     </main>
     <div class="flex w-full p-4">
-      <input class="bg-gray-900 flex-grow p-2" type="text" placeholder="Input"/>
-      <button class="ml-2 p-2 bg-gray-900 text-white">Send</button>
+      <input class="bg-gray-900 flex-grow p-2"
+             type="text"
+             placeholder="Input"
+             v-model="input"/>
+      <button class="ml-2 p-2 bg-gray-900 text-white" @click="onClick">Send</button>
     </div>
   </div>
 </template>
--- src/layout/LayoutSidebar.vue
+++ src/layout/LayoutSidebar.vue
@@ -3,12 +3,10 @@
 </script>
 
 <template>
-  <!-- Sidebar -->
   <div class="p-4 bg-gray-800 w-64 flex flex-col">
     <ul class="overflow-auto">
       <li>Menu Item 1</li>
       <li>Menu Item 2</li>
-      <!-- Repeat for as many menu items. -->
     </ul>
   </div>
 </template>
