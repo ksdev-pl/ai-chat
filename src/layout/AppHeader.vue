516da53c44d94bda0ed5b742285a9a5c47b5df52
ksdev <ks.cloudbox@gmail.com>
2024-04-01 13:19:01 +0200

    Layout WIP

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import {useAppStore} from '@/stores/app';
 
+const store = useAppStore();
 </script>
 
 <template>
   <div class="p-4 bg-gray-200">
-    Header
+    <button type="button" @click="store.toggleSidebar()">&#9776;</button>
   </div>
 </template>
--- src/layout/AppSidebar.vue
+++ src/layout/AppSidebar.vue
@@ -1,12 +1,35 @@
 <script setup lang="ts">
+import {useAppStore} from '@/stores/app';
 
+const store = useAppStore();
 </script>
 
 <template>
-  <div class="p-4 bg-gray-100 w-64 flex flex-col">
-    <ul class="overflow-auto">
-      <li>Menu Item 1</li>
-      <li>Menu Item 2</li>
-    </ul>
-  </div>
+  <Transition name="slide">
+    <div class="p-4 bg-gray-100 w-64 flex flex-col" v-if="store.isSidebarVisible">
+      <ul class="overflow-auto">
+        <li>Menu Item 1</li>
+        <li>Menu Item 2</li>
+      </ul>
+    </div>
+  </Transition>
 </template>
+
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform .5s ease;
+}
+.slide-enter-from {
+  transform: translateX(-100%);
+}
+.slide-enter-to {
+  transform: translateX(0);
+}
+.slide-leave-from {
+  transform: translateX(0);
+}
+.slide-leave-to {
+  transform: translateX(-100%);
+}
+</style>
