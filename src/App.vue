1d628837fd3e2fad66aabb42c9cf953609cab521
ksdev <ks.cloudbox@gmail.com>
2024-04-02 19:46:50 +0200

    Fun

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import LayoutSidebar from '@/layout/AppSidebar.vue';
-import LayoutContents from '@/layout/AppContents.vue';
-import LayoutHeader from '@/layout/AppHeader.vue';
+import AppSidebar from '@/layout/AppSidebar.vue';
+import AppContents from '@/layout/AppContents.vue';
+import AppHeader from '@/layout/AppHeader.vue';
 </script>
 
 <template>
   <div class="flex flex-col h-screen bg-white">
-    <LayoutHeader/>
+    <AppHeader/>
     <div class="flex flex-1 overflow-hidden">
-      <LayoutSidebar/>
-      <LayoutContents/>
+      <AppSidebar/>
+      <AppContents/>
     </div>
   </div>
 </template>
